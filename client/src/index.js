2b77564987146f6e2910407fe2db8fe253402268
Andrew <pemberton5150@gmail.com>
2020-05-16 13:53:12 -0600

    Added game engine

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from "./game";
+import * as ServiceWorker from './service-worker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+ReactDOM.render(<Game />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+ServiceWorker.register();
